e4ce14c215fd6703b7fec03310374c575be8b3ce
Kevin <Kevin.vandenabeele@ordina.be>
2019-10-17 18:09:47 +0200

    Fixed a bug when running on rbpi and not from terminal in dev environment
    Also updated the readme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const FFMPEG = require('./ffmpeg/ffmpeg').FFMPEG;
 
 const Unifi = require("./unifi/unifi").Unifi;
@@ -95,7 +97,7 @@ UnifiProtectCameraMotion.prototype.didFinishLaunching = function () {
 function setMotionCheckInterval(unifiConfig, unifiFlows, cameras, configuredAccessories) {
     if (unifiConfig.enhanced_motion) {
         Loader
-            .loadCoco(false, '../')
+            .loadCoco(false, path.dirname(require.resolve('homebridge-unifi-protect-camera-motion/package.json'))
             .then((detector) => {
                 setInterval(checkMotionEnhanced.bind(this,unifiConfig.enhanced_classes, unifiFlows, cameras, configuredAccessories, detector), unifiConfig.motion_interval);
             });
