21831b18cc50d55a7f463a33c757a35b17071201
Kevin <Kevin.vandenabeele@ordina.be>
2019-10-18 19:42:38 +0200

    Updated and extended readme. Added logic to annotate snapshots with bounding boxes
    bounding box logic is working, but still has to be implemented into the main code

--- src/coco/loader.ts
+++ src/coco/loader.ts
@@ -1,26 +1,26 @@
-import {loadImage, createCanvas, Image} from "canvas";
+const fs = require('fs');
+
+import {Canvas, createCanvas, Image, loadImage} from "canvas";
 import {createCocoModel, ObjectDetection} from "./coco";
 
 export class Loader {
 
     public static async loadCoco(useLiteModel: boolean, basePath?:string): Promise<Detector> {
         const model: ObjectDetection = await createCocoModel(useLiteModel, basePath);
-        const detector = {
+        return {
             async detect(image: Image, logResults: boolean = false): Promise<Detection[]> {
                 const start = Date.now();
 
-                const canvas = Loader.createCanvasFromImage(image);
-                const results = await model.detect(canvas);
+                const canvas: Canvas = Loader.createCanvasFromImage(image);
+                const results = await model.detect(canvas as unknown as HTMLCanvasElement);
 
                 if (logResults) {
                     Loader.printProcessDuration('COCO', start);
                     Loader.printResults(results);
                 }
-
-                return Promise.resolve(results);
+                return results;
             }
         };
-        return Promise.resolve(detector);
     }
 
     public static async createImage(pathOrUrl: string): Promise<Image> {
@@ -31,11 +31,34 @@ export class Loader {
         }
     }
 
-    public static createCanvasFromImage(image: Image): HTMLCanvasElement {
-        const canvas: HTMLCanvasElement = createCanvas(image.width, image.height) as unknown as HTMLCanvasElement;
+    public static createCanvasFromImage(image: Image): Canvas {
+        const canvas: Canvas = createCanvas(image.width, image.height);
         const ctx = canvas.getContext('2d');
-        ctx.drawImage(image as unknown as HTMLImageElement, 0, 0, image.width, image.height);
+        ctx.drawImage(image, 0, 0, image.width, image.height);
         return canvas;
+    }
+
+    public static saveAnnotatedImage(image: Image, detections: Detection[]) {
+        const canvas: Canvas = Loader.createCanvasFromImage(image);
+        const ctx = canvas.getContext('2d');
+        for (const detection of detections) {
+            Loader.drawRect(ctx, detection.bbox);
+        }
+        Loader.saveImage(canvas);
+    }
+
+    private static drawRect(ctx: any, bbox: number[]) {
+        ctx.strokeStyle = 'rgba(255,0,0,1)';
+        ctx.beginPath();
+        ctx.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
+        ctx.stroke();
+    }
+
+    private static  saveImage(canvas: Canvas) {
+        const out = fs.createWriteStream(__dirname + '/snapshot-' + new Date().toISOString() + '.jpg');
+        const stream = canvas.createJPEGStream();
+        stream.pipe(out);
+        out.on('finish', () =>  console.log('The snapshot has been saved!'));
     }
 
     private static printProcessDuration(name: string, start: number): void {
@@ -53,6 +76,7 @@ export class Loader {
 export interface Detection {
     class: string;
     score: number;
+    bbox: number[];
 }
 
 export interface Detector {
