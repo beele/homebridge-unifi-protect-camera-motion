dd9b4a8060b265ada0852c321f5345fd33d07508
Kevin Van den Abeele <kevin.vandenabeele@gmail.com>
2020-05-12 19:19:36 +0200

    Upgrade tfjs & script to fix arm build

---
+++ scripts/postinstall.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+switch (process.arch) {
+    case 'arm':
+    case 'arm64':
+        fixTensorFlowForArm();
+        break;
+    case 'x32':
+    case 'x64':
+        console.log('Supported architecture, no actions required!');
+        fixTensorFlowForArm();
+        break;
+    default:
+        console.error('Unsupported processor architecture!');
+}
+
+function fixTensorFlowForArm() {
+    const content = {
+        "tf-lib": "https://s3.us.cloud-object-storage.appdomain.cloud/tfjs-cos/libtensorflow-cpu-linux-arm-1.15.0.tar.gz"
+    };
+
+    if (fs.existsSync(process.cwd() + '/node_modules/@tensorflow/tfjs-node/scripts/')) {
+        fs.writeFileSync(process.cwd() + '/node_modules/@tensorflow/tfjs-node/scripts/custom-binary.json', JSON.stringify(content, null, 4));
+
+        exec('npm install', {cwd: process.cwd() + '/node_modules/@tensorflow/tfjs-node/'}, (error, stdout, stderr) => {
+            console.log(stdout);
+            console.error(stderr);
+        });
+    }
+}
