4219400c2db65d2175b8cb3aefa6b56e86f584ac
Donavan Becker <9875439+donavanbecker@users.noreply.github.com>
2020-06-24 11:33:11 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import {API} from "homebridge";
 import {UnifiProtectMotionPlatform} from "./unifi-protect-motion-platform";
 import {PLATFORM_NAME} from "./settings";
+import {PLUGIN_NAME} from "./settings";
 
 export = (api: API) => {
-    api.registerPlatform("homebridge-unifi-protect-camera-motion", PLATFORM_NAME, UnifiProtectMotionPlatform);
+    api.registerPlatform("PLUGIN_NAME", PLATFORM_NAME, UnifiProtectMotionPlatform);
 }
