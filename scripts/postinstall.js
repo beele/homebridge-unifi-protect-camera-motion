e2c0b4298cc6d8a8c6319e76b61ca0a29ce883f8
Kevin Van den Abeele <kevin.vandenabeele@gmail.com>
2020-08-13 20:16:22 +0200

    Hoobs install should be working now

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const exec = require('child_process').exec;
 
-console.log('homebridge-unifi-protect-camera-motion postinstall script running on: ' + process.arch);
+console.log('homebridge-unifi-protect-camera-motion post install script running on: ' + process.arch);
 switch (process.arch) {
     case 'arm':
-        console.log('ARM architecture, tfjs-lib not precompiled, downloading external precompiled lib...');
+        console.log('ARM architecture, tfjs-lib not precompiled, specifying external precompiled lib...');
         console.log('Specific ARM version: ' + process.config.variables.arm_version);
 
         createCustomBinaryJson('https://s3.us.cloud-object-storage.appdomain.cloud/tfjs-cos/libtensorflow-cpu-linux-arm-1.15.0.tar.gz');
@@ -30,7 +30,8 @@ function createCustomBinaryJson(packageUrl) {
     let tfjsNodeFolder = process.cwd() + '/node_modules/@tensorflow/tfjs-node/';
     let tfjsNodeFolderFound = fs.existsSync(tfjsNodeFolder);
     if (!tfjsNodeFolderFound) {
-        tfjsNodeFolder = process.cwd() + '../../node_modules/@tensorflow/tfjs-node/';
+        // Move up in the folder tree two levels.
+        tfjsNodeFolder = tfjsNodeFolder.replace('homebridge-unifi-protect-camera-motion/node_modules/', '')
         tfjsNodeFolderFound = fs.existsSync(tfjsNodeFolder);
     }
 
@@ -38,15 +39,15 @@ function createCustomBinaryJson(packageUrl) {
         console.log('Writing custom binary definition in: ' +  tfjsNodeFolder + 'scripts/');
         fs.writeFileSync(tfjsNodeFolder + 'scripts/custom-binary.json', JSON.stringify(content, null, 4));
 
-        exec('npm install', {cwd: tfjsNodeFolder}, (error, stdout, stderr) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            console.log(stdout);
-            console.error(stderr);
+        console.log('Running install for tfjs-node again with custom binary specified');
+        const installProcess = exec('sudo npm install --unsafe-perm=true', {cwd: tfjsNodeFolder});
+
+        installProcess.stdout.pipe(process.stdout);
+        installProcess.stderr.pipe(process.stderr);
+        installProcess.on('exit', (code) => {
+            console.log('npm install for tfjs-node exited with code ' + code.toString());
         });
     } else {
-        console.log('Folder does not exist: ' + tfjsNodeFolderFound);
+        console.log('Folder does not exist: ' + tfjsNodeFolder);
     }
 }
