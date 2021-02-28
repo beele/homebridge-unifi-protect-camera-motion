21fb037f3047ddbf3a04676e123deb7bdd79d06c
Kevin Van den Abeele <kevin.vandenabeele@gmail.com>
2021-02-28 11:11:21 +0100

    Updated postinstall, added mqtt dependency

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,15 +4,18 @@ const exec = require('child_process').exec;
 console.log('homebridge-unifi-protect-camera-motion post install script running on: ' + process.arch);
 switch (process.arch) {
     case 'arm':
-        console.log('ARM architecture, tfjs-lib not precompiled, specifying external precompiled lib...');
+        console.log('Supported architecture, tfjs-lib should be available!');
         console.log('Specific ARM version: ' + process.config.variables.arm_version);
 
-        createCustomBinaryJson('https://s3.us.cloud-object-storage.appdomain.cloud/tfjs-cos/libtensorflow-cpu-linux-arm-1.15.0.tar.gz');
+        // console.log('ARM architecture, tfjs-lib not precompiled, specifying external precompiled lib...');
+        // createCustomBinaryJson('https://s3.us.cloud-object-storage.appdomain.cloud/tfjs-cos/libtensorflow-cpu-linux-arm-1.15.0.tar.gz');
         break;
     case 'arm64':
-        console.log('ARM64 architecture, tfjs-lib not precompiled, downloading external precompiled lib...');
+        console.log('Supported architecture, tfjs-lib should be available!');
+        console.log('Specific ARM version: ' + process.config.variables.arm_version);
 
-        createCustomBinaryJson('https://s3.us.cloud-object-storage.appdomain.cloud/tfjs-cos/libtensorflow-gpu-linux-arm64-1.15.0.tar.gz');
+        // console.log('ARM64 architecture, tfjs-lib not precompiled, downloading external precompiled lib...');
+        // createCustomBinaryJson('https://s3.us.cloud-object-storage.appdomain.cloud/tfjs-cos/libtensorflow-gpu-linux-arm64-1.15.0.tar.gz');
         break;
     case 'x32':
     case 'x64':
@@ -22,6 +25,7 @@ switch (process.arch) {
         console.error('Unsupported processor architecture: ' + process.arch);
 }
 
+// TFJS seems to again provide prebuilt binaries for ARM (Raspberry Pi/...), so this is unused for the time being!
 function createCustomBinaryJson(packageUrl) {
     const content = {
         "tf-lib": packageUrl
